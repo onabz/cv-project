de2aedad99f9747a6e8e7da997aa2546163ef511
onabz <ONABOLUMBE@GMAIL.COM>
2022-02-25 18:56:42 +0000

    Add javascript

---
+++ script.js
@@ -0,0 +1,93 @@
+// const calcArea = function(radius){
+//     return area = 3.14 * radius**2;
+//   }
+  
+// const calcArea = radius => 3.14 * radius**2;
+
+//   let cal = calcArea(50);
+//   console.log(cal);
+//   console.log(cal);
+
+
+// const greet = () => 'hello, mum'; 
+
+// console.log(greet());
+
+// let name = 'olumbe';
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+// console.log(bill([10,15,30], 0.2));
+
+
+// let resultA = name.toUpperCase();
+// console.log(resultA);
+
+
+// function myFunction(p1, p2){
+//     return p1 * p2;
+// }
+
+// let result = myFunction(10, 50);
+// console.log(result);
+
+// function myFunction(a, b) {
+//     return a * b;
+// }
+
+// let result = myFunction(6, 10);
+// console.log(result);
+
+
+// function toCelcius(fahrenheit) {
+//     return (5/9) * (fahrenheit - 32);
+// }
+
+// let result = toCelcius(108);
+// console.log(result);
+
+const person = {
+    firstName: "Olumbe",
+    lastName: "Onabolu",
+    age: "50",
+    eyecolor: "black"
+};
+
+console.log(person["firstName"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
